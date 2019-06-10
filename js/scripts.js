cf0b31f77e57f493e319ac217e50d87f6c92102c
Anna Gritsai and Marqis Ramos <marqis@>
2019-06-10 16:52:27 -0700

    deck of cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,47 @@
 $(document).ready(function() {
-  $("form#list").submit(function(event) {
-    var blanks = ["item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8"];
-    var empty = [];
+//   $("form#list").submit(function(event) {
+//     var blanks = ["item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8"];
+//     var empty = [];
+//
+//
+//
+//     blanks.forEach(function(item) {
+//       var userInput = $("input#" + item).val();
+//       empty.push(userInput.toUpperCase());
+//
+//
+//     });
+//
+//     empty.sort();
+//
+//     empty.forEach(function(whatever) {
+//       $("#complete-list").append("<li>" + whatever +"</li>" );
+//     });
+//
+//     $("#complete-list").show();
+//     $("#list").hide();
+//
+//       // // $("#complete-list").show();
+//       // // $("#list").hide();
+//       // array.forEach(function(array){
+//       //   $("#complete-list").append("<li>" +array+"</li>" );
+//       // });
+//
+// event.preventDefault();
+//     });
+//
 
 
+   var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+   var suits = ["spades", "diamonds", "clubs", "hearts"];
+   var deck = [];
 
-    blanks.forEach(function(item) {
-      var userInput = $("input#" + item).val();
-      empty.push(userInput.toUpperCase());
+   suits.forEach(function(suit) {
+     cards.forEach(function(card) {
+       deck.push(card + "of" + suit);
 
 
-    });
-
-    empty.sort();
-
-    empty.forEach(function(whatever) {
-      $("#complete-list").append("<li>" + whatever +"</li>" );
-    });
-
-    $("#complete-list").show();
-    $("#list").hide();
-
-      // // $("#complete-list").show();
-      // // $("#list").hide();
-      // array.forEach(function(array){
-      //   $("#complete-list").append("<li>" +array+"</li>" );
-      // });
-
-event.preventDefault();
-    });
+     });
+   });
+console.log(deck);
   });
